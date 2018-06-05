7b0597947a51757a4cef5a67fcde725181e0a3be
Erik Goossens <erikmgoossens@gmail.com>
2018-06-05 16:58:13 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Erik Goossens on 2018-06-05.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
